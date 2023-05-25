3dd1a7348676a1f755750ac0150d38192cbbc1f5
Alan Enrique Galavis Du Bois <u202110223@upc.edu.pe>
2023-05-25 11:16:16 -0500

    Added requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, url_for
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
 #flask --app app.py --debug run
+
+#En el terminal hacer lo siguiente:
+"""
+pip install pipenv
+pipenv --python 3.8
+pipenv install -r requirements.txt
+pipenv shell
+python app.py
+"""
