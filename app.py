75b8065fbb49b5f98cb81c14d74c46cb56feb975
CarloLSG <u20211a475@upc.edu.pe>
2023-06-09 23:24:36 -0500

    Version 2.0
    Solucion de bugs

--- app.py
+++ app.py
@@ -3,7 +3,38 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
+<<<<<<< Updated upstream
 formData = {}
+=======
+generate_weights.generar_csvs()
+df_movies = pd.read_csv('Amdb_5000_movies.csv')
+movie_dataset = df_movies['original_title'].astype(str).tolist()
+movie_dataset_minusculas = [x.lower() for x in movie_dataset]
+
+#para los posters
+df_posters = pd.read_csv('pmdb_5000_movies.csv')
+
+movie_name = df_posters['original_title'].astype(str).tolist()
+id_dataset = df_posters['id'].astype(int).tolist()
+
+movie_name_minusculas = [x.lower() for x in movie_name]
+
+posters = dict(zip(movie_name_minusculas, id_dataset))
+
+def get_poster(movie_id_list):
+    full_path = []
+    for movie_id in movie_id_list:
+        try:
+            url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+            data = requests.get(url)
+            data = data.json()
+            poster_path = data['poster_path']
+            full_path.append("https://image.tmdb.org/t/p/w500/" + poster_path)
+        except:
+            print("Error, movie poster not found")
+            full_path.append("https://pbs.twimg.com/media/FyOunJ_WYAAXBwX?format=jpg&name=small")
+    return full_path
+>>>>>>> Stashed changes
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
---
+++ generate_weights.py
@@ -0,0 +1,194 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+import csv
+import ast
+import os.path
+
+rango = 4449
+df_movies = pd.read_csv('Amdb_5000_movies.csv')
+
+#Overview
+headers = ["Source", "Target", "Weight"]
+tfidf = TfidfVectorizer(stop_words="english")
+df_movies['overview']=df_movies['overview'].fillna("")
+tfid_matrix = tfidf.fit_transform(df_movies['overview'])
+
+cosine_sim = linear_kernel(tfid_matrix, tfid_matrix)
+
+indices = pd.Series(df_movies.index, index = df_movies['id']).drop_duplicates()
+
+columna_lista = df_movies['vote_average'].astype(float).tolist()
+
+def generate_overview_recomendations(id, cosine_sim = cosine_sim, cont = 0):
+    lis = []
+    lista = []
+    idx = indices[id]
+    sim_scores = cosine_sim[idx]
+    for i in sim_scores:
+        #i = 10
+        i=round(i*100)
+        lista = [id,cont,i+columna_lista[cont]]
+        if id == cont:
+            cont += 1
+            continue
+        if i <= np.float64(7.0):
+            cont +=1
+            continue
+        #validar que no se hagan relaciones cuando es 0.0
+        cont += 1
+        lis.append(lista)
+        #print(i)
+    return lis
+
+def get_recomendations_overview():
+    file = open("edges_list_overview.csv", "a", newline='')
+    writer = csv.writer(file)
+    
+    writer.writerow(headers)
+    for i in range(4449):
+        liston = generate_overview_recomendations(i)
+        writer.writerows(liston)
+    file.close()
+        
+def overview_is_empty():
+    df = pd.read_csv("edges_list_overview.csv")
+    return df.empty
+
+def generate_overview():
+    if os.path.exists("edges_list_overview.csv") is False:
+        get_recomendations_overview()
+    elif overview_is_empty():
+        get_recomendations_overview()
+
+
+#Generos
+def obtener_ids_por_elemento(input_str):
+    ids_por_elemento = []
+    data = ast.literal_eval(input_str)
+    for elemento in data:
+        if 'id' in elemento:
+            ids_por_elemento.append(int(elemento['id']))
+    return ids_por_elemento
+
+
+def crear_lista_genero():
+    elementos_genero = df_movies['genres'].tolist()
+
+    resultados_genero = []
+    for elemento in elementos_genero:
+        ids = obtener_ids_por_elemento(elemento)
+        resultados_genero.append(ids) 
+    return resultados_genero
+
+
+def generate_genre_recomendations(id, id_genres, cont = 1):
+    lis = []
+    listita=[]
+    for i in id_genres:
+        listita=[id,cont,i+columna_lista[cont]]
+        if (i+columna_lista[cont]) <= np.float64(10.4):
+            cont +=1
+            continue
+        cont += 1
+        lis.append(listita)
+    return lis
+
+def crear_listas_ids_genero(resultados_genero):
+    id_genres = [sum(valor in otra_lista for valor in resultados_genero[0]) for otra_lista in resultados_genero[1:]]
+    return id_genres
+
+def get_recomendations_genre():
+    resultados_genero = crear_lista_genero()
+    codigos_genero = crear_listas_ids_genero(resultados_genero)
+    file2 = open("genres_list.csv", "a", newline='')
+    writer2 = csv.writer(file2)
+
+    writer2.writerow(headers)
+
+    for i in range(4449):
+        liston2 = generate_genre_recomendations(i, codigos_genero)
+        writer2.writerows(liston2)
+    file2.close()
+    
+def genre_is_empty():
+    df = pd.read_csv("genres_list.csv")
+    return df.empty
+
+def generate_genre():
+    if os.path.exists("genres_list.csv") is False:
+        get_recomendations_genre()
+    elif genre_is_empty():
+        get_recomendations_genre()
+
+#PC
+def obtener_ids_por_elemento_PC(input_str_PC):
+    ids_por_elemento_PC = []
+    data_PC = ast.literal_eval(input_str_PC)
+    for elemento_PC in data_PC:
+        if 'id' in elemento_PC:
+            ids_por_elemento_PC.append(int(elemento_PC['id']))
+    return ids_por_elemento_PC
+
+def crear_lista_PC():
+    elementos_PC = df_movies['production_companies'].tolist()
+
+    resultados_PC = []
+    for elemento_PC in elementos_PC:
+        ids_PC = obtener_ids_por_elemento_PC(elemento_PC)
+        resultados_PC.append(ids_PC)
+    
+    return  resultados_PC
+
+def generate_PC_recomendations(id, ids_PC, cont = 1):
+    lis = []
+    listita=[]
+    for i in ids_PC:
+        listita=[id,cont,i+columna_lista[cont]]
+        if (i+columna_lista[cont]) <= np.float64(8.7):
+            cont +=1
+            continue
+        cont += 1
+        lis.append(listita)
+    return lis
+
+def crear_listas_ids_PC(resultados_PC):
+    id_PC = [sum(valor_PC in otra_lista_PC for valor_PC in resultados_PC[0]) for otra_lista_PC in resultados_PC[1:]]
+    return id_PC
+
+def get_recomendations_PC():
+    resultados_PC = crear_lista_PC()
+    codigos_PC = crear_listas_ids_PC(resultados_PC)
+    file3 = open("PC_list.csv", "a", newline='')
+    writer3 = csv.writer(file3)
+
+    writer3.writerow(headers)
+
+    for i in range(4449):
+        liston3 = generate_PC_recomendations(i, codigos_PC)
+        writer3.writerows(liston3)
+    file3.close()
+
+def PC_is_empty():
+    df = pd.read_csv("PC_list.csv")
+    return df.empty
+
+def generate_PC():
+    if os.path.exists("PC_list.csv") is False:
+        get_recomendations_PC()
+    elif PC_is_empty():
+        get_recomendations_PC()
+
+#Generador
+def generar_csvs():
+    
+    
+    #Overview.csv
+    generate_overview()
+
+    #Generos.csv
+    generate_genre()
+        
+    #PC.csv
+    generate_PC()
