4430dcb7668c80247f78ef88cb4359b26b0b57ae
CarloLSG <u20211a475@upc.edu.pe>
2023-06-20 23:24:00 -0500

    Graph Example for theory
    Graph Example for theory

--- DLS.py
+++ DLS.py
@@ -1,5 +1,6 @@
 import csv
 import os.path
+import graphviz as gv
 
 def return_weight(e):
   return e[1]
@@ -22,13 +23,12 @@ def addValue(lista, n, qty):
     lista.append(n)
   
 
-def dls(G, s, L, qty): #añadir parametro
+def dls(G, s, L, qty): 
   n = len(G)
   visited = [False]*n
   path = [None]*n
   cost = [float('inf')]*n
   lista = []
-  #Verificar si el número es mayor que los presentes en la lista y si es append, al final ordeno
   
   def _dls(u, L):
      if L > 0 and not visited[u]:
@@ -82,21 +82,21 @@ def overview_recomendation(index, qty, peliculas_lista):
         path, lista = dls(G_overview, index, 1, qty) #no retornar ni peso ni costo solo dejarlo como comentario para que se vea en la expo
         lista.sort(key=return_weight, reverse=True)
         movies_list = return_movies_titles(lista, peliculas_lista)
-        return movies_list
+        return movies_list, path
 
 def genre_recomendation(index, qty, peliculas_lista):
     if index != -1:
         path, lista = dls(G_genre, index, 1, qty) #no retornar ni peso ni costo solo dejarlo como comentario para que se vea en la expo
         lista.sort(key=return_weight, reverse=True)
         movies_list = return_movies_titles(lista, peliculas_lista)
-        return movies_list
+        return movies_list, path
 
 def PC_recomendation(index, qty, peliculas_lista):
     if index != -1:
         path, lista = dls(G_studio, index, 1, qty) #no retornar ni peso ni costo solo dejarlo como comentario para que se vea en la expo
         lista.sort(key=return_weight, reverse=True)
         movies_list = return_movies_titles(lista, peliculas_lista)
-        return movies_list
+        return movies_list, path
 
 def filter_selector(movieReference, movieSelectedFilter, movieRecommendationAmount, movie_dataset):
   try:
@@ -104,10 +104,65 @@ def filter_selector(movieReference, movieSelectedFilter, movieRecommendationAmou
   except:
     index = -1
     return ["Error, movie not found in the dataset"]
-  
+  output_filename = "teoria.png"
   if movieSelectedFilter == "studio":
-    return PC_recomendation(index, movieRecommendationAmount, movie_dataset)
+    movie_list, path = genre_recomendation(index, movieRecommendationAmount, movie_dataset)
+    subgrafo = createSubgraph(G_studio, index)
+    drawGraph(subgrafo, quantity=movieRecommendationAmount)
+    return movie_list
   elif movieSelectedFilter == "genre":
-    return genre_recomendation(index, movieRecommendationAmount, movie_dataset)
+    movie_list, path = genre_recomendation(index, movieRecommendationAmount, movie_dataset)
+    subgrafo = createSubgraph(G_genre, index)
+    drawGraph(subgrafo, quantity=movieRecommendationAmount)
+    return movie_list
   elif movieSelectedFilter == "overview":
-    return overview_recomendation(index, movieRecommendationAmount, movie_dataset)
+    movie_list, path = overview_recomendation(index, movieRecommendationAmount, movie_dataset)
+    subgrafo = createSubgraph(G_overview, index)
+    drawGraph(subgrafo, quantity=movieRecommendationAmount)
+    return movie_list
+  
+
+def drawGraph(L, filename="graph", quantity=None):
+    if os.path.exists(filename):
+      os.remove(filename)
+    
+    graph = gv.Digraph("G")
+    graph.attr('graph', layout='sfdp')
+    graph.attr('node', color='orangered', fontcolor='mediumslateblue', fontname='monospace', fontsize='8', height='0.1', width='0.1')
+    graph.attr('edge', color='gray', fontname='monospace', fontsize='8')
+    
+    nodes = set()
+    for i, connection in enumerate(L):
+        source, target, weight = connection
+        nodes.add(source)
+        nodes.add(target)
+        
+        if i < quantity:
+            graph.attr('node', color='orangered', fontcolor='mediumslateblue')
+            graph.edge(str(source), str(target), label=str(weight), color='orange', penwidth='2')
+        else:
+            graph.attr('node', color='black', fontcolor='black')
+            graph.edge(str(source), str(target), label=str(weight))
+    
+    for node in nodes:
+        graph.node(str(node), label=str(node))
+
+    graph.render(filename, format= "png")
+
+    
+def createSubgraph(graph, source_node, path=[]):
+    subgraph = []
+
+    for src, edges in graph.items():
+        if src == source_node:
+            for dst, weight in edges:
+                subgraph.append([src, dst, weight])
+    
+    orderbyweight(subgraph)
+    return subgraph
+  
+def orderbyweight(subgraph):
+  subgraph.sort(key=return_subgraph_weight, reverse=True)
+
+def return_subgraph_weight(e):
+  return e[2]
